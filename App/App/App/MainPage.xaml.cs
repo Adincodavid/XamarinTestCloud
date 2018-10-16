e9b67cc3907d2161738bc89b9ff61a7d10c0b41b
Luis David De La Cruz Bautista <david.cruz@ogss.com.mx>
2018-10-16 13:55:27 -0500

    Carousel Getting the current selected item

--- App/App/App/MainPage.xaml.cs
+++ App/App/App/MainPage.xaml.cs
@@ -15,33 +15,58 @@ namespace App
         {
             "Node js","C#","Java","SQL Server","Oracle","Web Services","Cosmos DB","Mongo DB","Entity Framework",".Net Core","Visual Basic .Net","Steve Jobs","Bill Gates","Mac OS","Elementary OS",
         };
+        TimeWrapper timer;
+        private DateTime _timerStart;
         public MainPage()
         {
             InitializeComponent();
+           
             SfCarousel carousel = new SfCarousel() { ItemWidth = 170, ItemHeight = 250 };
             ObservableCollection<SfCarouselItem> collectionOfItems = new ObservableCollection<SfCarouselItem>();
-            collectionOfItems.Add(new SfCarouselItem() { ImageName = "area.png" });
-            collectionOfItems.Add(new SfCarouselItem() { ImageName = "donut.png" });
-            collectionOfItems.Add(new SfCarouselItem() { ImageName = "pie.png" });
-            collectionOfItems.Add(new SfCarouselItem() { ImageName = "series.png" });
-            collectionOfItems.Add(new SfCarouselItem() { ImageName = "step.png" });
+            collectionOfItems.Add(new SfCarouselItem() { ImageName = "area.png", ClassId ="1" });
+            collectionOfItems.Add(new SfCarouselItem() { ImageName = "donut.png" , ClassId = "2" });
+            collectionOfItems.Add(new SfCarouselItem() { ImageName = "pie.png", ClassId = "3" });
+            collectionOfItems.Add(new SfCarouselItem() { ImageName = "series.png", ClassId = "4" });
+            collectionOfItems.Add(new SfCarouselItem() { ImageName = "step.png", ClassId = "5" });
             carousel.ItemsSource = collectionOfItems;
             this.Content = new StackLayout()
             {
                 Children = { carousel, Activity,ListaGenerica }
             };
             carousel.SelectedIndex = 1;
-
+            List<string> Numeros = new List<string>();
             carousel.SelectionChanged += async (object sender, SelectionChangedEventArgs e) =>
             {
+                var obj = (SfCarouselItem)e.SelectedItem;
+                string Id = obj.ClassId;
+                string Nombre = obj.ImageName;
+                ListaGenerica.ItemsSource = null;
                 await Task.Yield();
                 Activity.IsRunning = true;
                 await Task.Delay(3000);
+                ListaGenerica.ItemsSource = elementoslista;
                 Activity.IsRunning = false;
-            
+                Numeros.Add(Id);
+                var c = Numeros[Numeros.Count - 1];
+                await DisplayAlert("Ok", $"{Id},{Nombre}, {c.ToString()}","OK");
             };
-            ListaGenerica.ItemsSource = elementoslista;
+        }
+        private void TimerElapsedEvt()
+        {
+            var secondsSinceStart = GetSecondsSinceTimerStart;
+            if (secondsSinceStart % 2 == 0)
+            {
+                
+            }
         }
 
+        private int GetSecondsSinceTimerStart
+        {
+            get
+            {
+                TimeSpan lapsedTime = DateTime.Now - _timerStart;
+                return lapsedTime.Seconds;
+            }
+        }
     }
 }
